dbe8eae1e98991f46493c38a12ce78a7370a38bb
Nishkarsh <nishkarshs33@gmail.com>
2025-06-29 16:20:27 +0000

    sanity-tes

--- test/test_model.py
+++ test/test_model.py
@@ -1,15 +1,20 @@
+import unittest
 import joblib
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-def test_model_accuracy():
-    df = pd.read_csv('data/iris.csv')
-    X = df.drop('species', axis=1)
-    y = df['species']
-    _, X_test, _, y_test = train_test_split(X, y, test_size=0.2)
+class TestModel(unittest.TestCase):
+    def test_accuracy(self):
+        df = pd.read_csv('data/iris.csv')
+        X = df.drop('species', axis=1)
+        y = df['species']
+        _, X_test, _, y_test = train_test_split(X, y, test_size=0.2)
 
-    model = joblib.load('model.joblib')
-    y_pred = model.predict(X_test)
+        model = joblib.load('model.joblib')
+        y_pred = model.predict(X_test)
 
-    assert accuracy_score(y_test, y_pred) > 0.7
+        self.assertGreater(accuracy_score(y_test, y_pred), 0.7)
+
+if __name__ == '__main__':
+    unittest.main()
