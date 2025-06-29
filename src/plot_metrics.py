faa4ebb71b732aa8c03c4eb7d14610a6f6017dbc
Nishkarsh <nishkarshs33@gmail.com>
2025-06-29 16:38:42 +0000

    sanity-tes

--- src/plot_metrics.py
+++ src/plot_metrics.py
@@ -4,13 +4,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('data/iris.csv')
-X = df.drop('species', axis=1)
-y = df['species']
+df = pd.read_csv("data/iris.csv")
+X = df.drop("species", axis=1)
+y = df["species"]
 
 _, X_test, _, y_test = train_test_split(X, y, test_size=0.2)
 
-model = joblib.load('model.joblib')
+model = joblib.load("model.joblib")
 y_pred = model.predict(X_test)
 
 cm = confusion_matrix(y_test, y_pred)
--- src/train.py
+++ src/train.py
@@ -4,16 +4,16 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 import joblib
 
-df = pd.read_csv('data/iris.csv')
-X = df.drop('species', axis=1)
-y = df['species']
+df = pd.read_csv("data/iris.csv")
+X = df.drop("species", axis=1)
+y = df["species"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 model = DecisionTreeClassifier()
 model.fit(X_train, y_train)
 
-joblib.dump(model, 'model.joblib')
+joblib.dump(model, "model.joblib")
 
 y_pred = model.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
